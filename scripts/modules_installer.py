b352d7b2a89bc7e77b8c5dbc10d98399ffdbf090
enzo <enzo.ccc@gmail.com>
2020-08-22 14:12:22 +0200

    Add files via upload

--- scripts/modules_installer.py
+++ scripts/modules_installer.py
@@ -39,11 +39,12 @@ if not packages:
         'PypeR',
         'graphviz==0.8.3',
         'pysftp',
-        'xlsxwriter',
+        'xlsxwriter',        
         'pandas',
-        'ffmpeg',
         'opencv-python'
+        
     ]
+
 python_path = sys.exec_prefix
 python_version = sys.version[:3]
 
@@ -59,3 +60,4 @@ subprocess.check_call([cmd, '-m', 'ensurepip'], shell=False)
 
 for p in packages:
     subprocess.check_call([cmd, '-m', 'pip', 'install', '--upgrade', p,'--user'], shell=False)
+    
